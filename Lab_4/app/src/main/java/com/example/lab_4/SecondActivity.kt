625a21191335d0854d8721e6c5fc91a972d23022
AndreyFortus <andrey020904@gmail.com>
2024-04-24 20:44:59 +0300

    add lab 4

---
+++ Lab_4/app/src/main/java/com/example/lab_4/SecondActivity.kt
@@ -0,0 +1,13 @@
+package com.example.lab_4
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+    }
+}
